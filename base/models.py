68983374805d1030a58328e275f7f9ee5efa957b
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2022-12-17 16:25:29 +0200

    adding some models:

--- base/models.py
+++ base/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self) :
+        return self.name
+
 
 # Create your models here.
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=100)
+    descritpion = models.TextField(null=True, blank=True)
+    # this attr can't be blank (in database table)
+    # participents
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    # auto_now -> take a date snapshot everytime this updated
+    # auto_now_add -> take a date snanshot only once this created
+
+    def __str__(self):
+        return self.name
+
+
+# Many to one relationship with room
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.body[0:50]
